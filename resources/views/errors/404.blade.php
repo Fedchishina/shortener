db7674f430724f9bfcd3387c9b075e6b553723f8
Fedchishina Victoria <fedchishina.victoria@gmail.com>
2017-07-18 21:35:03 +0300

    added 404 and 500 errors

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.app')
+@section('content')
+    <h1 class="index-title">Sorry, the page you are looking for could not be found.</h1>
+@endsection
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.app')
+@section('content')
+    <h1 class="index-title">Whoops... Looks like something went wrong. 500 error</h1>
+@endsection
